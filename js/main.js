b75ee2972ea57a8135fcceecc66e62ea8d44858e
Joy Xie <a556987@gmail.com>
2020-04-15 14:17:37 +0800

    add function

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ $(function(){
         ['E3', 'F3', '#F3/bG3', 'G3', '#G3/bA3', 'A3', '#A3/bB3', 'B3', 'C4', '#C4/bD4', 'D4', '#D4/bE4', 'E4', 'F4', '#F4/bG4', 'G4', '#G4/bA4', 'A4', '#A4/bB4', 'B4', 'C5', '#C5/bD5']  // 第六弦
     ]
 
+    // color
     let notes_array_3 = [
         ['5', '5', '5', '5', '5', '5', '5', '5', '6', '6', '6', '6', '6', '6', '6', '6', '6', '6', '6', '6', '7', '7'], // 第一弦
         ['4', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '6', '6', '6', '6', '6', '6', '6', '6', '6'], // 第二弦
@@ -38,12 +39,7 @@ $(function(){
     ]
 
     let $note_panel = $('.note-panel');
-
-    // 軌道
-    let line_html = `<div class="line"></div>`;
-
-    // 格子組
-    let note_group_html = `<div class="note-wrap"></div>`;
+    let $add_line_btn = $('.add');
 
     // 選單
     let menu_html = `<div class="grid"></div><div class="menu"><select name="note"></select><div class="ok">V</div><div class="cancel">X</div></div>`;
@@ -51,55 +47,110 @@ $(function(){
     /* 建立軌道 */
     function addNoteLine() {
         // 面版加入軌道
-        $note_panel.append(line_html);
+        $note_panel.append(`<div class="line"><div class="button del">刪除</div></div>`);
         let $line = $('.line').last();
 
         // 軌道加入音符組
         for (let i=0 ; i < 20 ; i++) {
-            $line.append(note_group_html);
+            $line.append(`<div class="note-wrap"></div>`);
             let $cur_note_wrap = $line.find('.note-wrap').last();
 
             // 音符組加入內容
             for (let j=0 ; j < 6 ; j++) {
                 if (j < 3) {
-                    $cur_note_wrap.append(`<div class="chord treble">${menu_html}</div>`);
+                    $cur_note_wrap.append(`<div data-chord="${j+1}" class="chord treble">${menu_html}</div>`);
                 } else {
-                    $cur_note_wrap.append(`<div class="chord bass">${menu_html}</div>`);
+                    $cur_note_wrap.append(`<div data-chord="${j+1}" class="chord bass">${menu_html}</div>`);
                 }
 
                 // 選單中加入選項
                 let $cur_select = $cur_note_wrap.find('.menu select').last();
-                $cur_select.append('<option data-chord="none" value="none">none</option>');
+                $cur_select.append('<option value="none">none</option>');
                 for (let k=0 ; k < 22 ; k++) {
-                    $cur_select.append(`<option data-chord="${j+1}" data-note="${notes_array_2[j][k]}" class="note-${notes_array_3[j][k]}" value="${notes_array[j][k]}">${k}</option>`);
+                    $cur_select.append(`<option class="note-${notes_array_3[j][k]}" value="${k}">${k}</option>`);
                 }
             }
         }
+
+        /* Event Binding */
+        $('.grid').each(function(index){
+            /* grid */
+            $(this).on('click', function(event){
+                // open the menu
+                $('.menu').removeClass('active');
+                $(this).siblings('.menu').addClass('active');
+            });
+
+            /* menu - ok */
+            $(this).siblings('.menu').find('.ok').on('click', function(event){
+                // 取得下拉式選單的 value
+                let grid = $(this).siblings('select').val();
+                
+                // 清除音符
+                $(this).parents('.menu').siblings('.grid').find('.note').remove();
+
+                // 檢查
+                if (grid != "none") {
+                    let chord = $(this).parents('.chord').attr('data-chord');
+                    let note_s = notes_array[chord - 1][grid];
+                    let note_t = notes_array_2[chord - 1][grid];
+                    let note_color = notes_array_3[chord - 1][grid];
+
+                    $(this).parents('.menu').siblings('.grid').append(`<span class="note note-${note_color}" data-note="${note_t}">${note_s}</span>`);
+                }
+                
+                // closed the menu
+                $(this).parents('.menu').removeClass('active');
+            });
+
+            /* menu - cancel */
+            $(this).siblings('.menu').find('.cancel').on('click', function(event){
+                // closed the menu
+                $(this).parents('.menu').removeClass('active');
+            });
+        });
+
+        /* del line btn */
+        $line.find('.del').on('click', function(event){
+            // 檢查
+            if ($note_panel.find('.line').length > 1) {
+                $(this).parents('.line').remove();
+            }
+        });
     }
+
+    /* 修改歌名 */
+    $('.title').on('dblclick', function(event){
+        var pre_name = $('.title').html(); // 預設值為上次輸入的歌名
+        var decodeHtml = htmlDecode(pre_name); //用浏览器内部转换器实现html解码
+        var song_name = prompt('請輸入歌名：', decodeHtml);
+        
+        if(song_name == null){
+            song_name = decodeHtml;
+        }
+        $('.title').text(song_name); // 顯示新歌名
+    });
 
     /* 初始化 */
     addNoteLine(); // 建立軌道
 
-
-    /* Event Binding */
-    $('.grid').each(function(index){
-        /* grid */
-        $(this).on('click', function(event){
-            // open the menu
-            $('.menu').removeClass('active');
-            $(this).siblings('.menu').addClass('active');
-        });
-
-        /* menu - ok */
-        $(this).siblings('.menu').find('.ok').on('click', function(event){
-            // closed the menu
-            $(this).parents('.menu').removeClass('active');
-        });
-
-        /* menu - cancel */
-        $(this).siblings('.menu').find('.cancel').on('click', function(event){
-            // closed the menu
-            $(this).parents('.menu').removeClass('active');
-        });
+    /* add line btn */
+    $add_line_btn.on('click', function(event){
+        addNoteLine(); // 建立軌道
     });
+
+    //---------------------------------------------
+    // 用浏览器内部转换器实现html解码
+    // 來源: https://www.cnblogs.com/xdp-gacl/p/3722642.html
+    //---------------------------------------------
+    function htmlDecode(text){
+        //1.首先动态创建一个容器标签元素，如DIV
+        var temp = document.createElement("div");
+        //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
+        temp.innerHTML = text;
+        //3.最后返回这个元素的innerText(ie支持)或者textContent(火狐，google支持)，即得到经过HTML解码的字符串了。
+        var output = temp.innerText || temp.textContent;
+        temp = null;
+        return output;
+    }
 });
